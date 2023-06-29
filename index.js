6ea8a3e1c85e2fe0634099ede6cc4ea0fec1ea4c
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-29 16:29:28 +0200

    added connection to db correctly

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express')
+const {MongoClient} = require('mongodb')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-
 app.use(cors())
 app.use(express.static(__dirname + '/public/'))
+
+const client = new MongoClient(process.env.DB_URI);
+const db = client.db('exercise_tracker')
+
+const newUser = (req) => {
+  db
+    ? console.log('Conectado a la BBDD')
+    : console.log('No conectado')
+}
+
+const newExercise = (req) => {
+  db
+    ? console.log('Conectado a la BBDD')
+    : console.log('No conectado')
+}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 app.post('/api/users', (req, res) => {
+  newUser(req)
   res.json({newUser:'newUser'})
 });
 app.post('/api/users/:_id/exercises', (req, res) => {
+  newExercise(req)
   res.json({addExer:'addExer'})
 });
 
