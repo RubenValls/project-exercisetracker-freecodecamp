6f44aaefd3db778800dcc0faf595ed3d3dc1b59e
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-29 22:31:35 +0200

    users post working correctly

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 app.use(cors())
+app.use(express.urlencoded());
+app.use(express.json());
 app.use(express.static(__dirname + '/public/'))
 
 const client = new MongoClient(process.env.DB_URI);
 const db = client.db('exercise_tracker')
 
-const newUser = (req) => {
-  db
-    ? console.log('Conectado a la BBDD')
-    : console.log('No conectado')
+const newUser = async (req) => {
+  const users = db.collection('users')
+  const _user = req?.body?.username
+  
+  const response = await users.insertOne({username: _user})
+
+
+  const userAdded = {
+    username : _user,
+    _id : response?.insertedId.toString()  
+  }
+
+  return userAdded
 }
 
 const newExercise = (req) => {
@@ -25,8 +36,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 app.post('/api/users', (req, res) => {
-  newUser(req)
-  res.json({newUser:'newUser'})
+  newUser(req).then((response) => {
+    res.json(response)
+  })
 });
 app.post('/api/users/:_id/exercises', (req, res) => {
   newExercise(req)
